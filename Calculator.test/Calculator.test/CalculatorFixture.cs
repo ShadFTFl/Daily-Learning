0c8a2cd6e3bf07acb5f6a5838c86a36b6e80e451
mrofiqul <rmd.rofiqulislam@gmail.com>
2014-09-02 16:17:06 +0600

    NUnit Test

---
+++ Calculator.test/Calculator.test/CalculatorFixture.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using CalculatorLib;
+using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculatorFixture
+    {
+
+        [Test]
+        public void AddTest()
+        {
+            Calculator aCalculator = new Calculator();
+            double result = aCalculator.Add(10.4, 2.3);
+            Assert.AreEqual(12.7,result);
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Subtract(10.4, 2.4);
+            Assert.AreEqual(8, result,0.01);
+
+        }
+        [Test]
+        public void AddTestForNegativeNumber()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Add(-5.4, 2.3);
+            Assert.AreEqual(-3.1, result, 0.01);
+
+        }
+        [Test]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+            double result = calculator.Divide(6.0, 3.0);
+            Assert.AreEqual(2.0, result,0.01); 
+        }
+
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroTest()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Divide(3, 0);
+        }
+    
+    }
+}
---
+++ Calculator.test/CalculatorLib/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+   public class Calculator
+    {
+       public double Add(double firstNo, double secondNo)
+       {
+           return firstNo + secondNo;
+       }
+
+       public double Subtract(double firstNo, double secondNo)
+       {
+           return firstNo-secondNo;
+       }
+
+       public double Divide(double firstNo, double secondNo)
+       {
+           if (secondNo == 0)
+           {
+              throw new DivideByZeroException("Divide By Zero is not Possible");
+           }
+           return firstNo / secondNo;
+       }
+    }
+}
